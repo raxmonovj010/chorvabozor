866baf6156211afe9fc16437654820e144078376
raxmonovj010 <raxmonovj010@gmail.com>
2026-06-16 23:44:45 +0500

    Remove Cloudinary, use image URLs instead

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@
 const nextConfig = {
   images: {
     remotePatterns: [
-      { protocol: "https", hostname: "res.cloudinary.com" },
       { protocol: "https", hostname: "lh3.googleusercontent.com" },
     ],
   },
